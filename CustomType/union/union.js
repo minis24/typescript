8bfa63904c88b92f8f3b4c57c8f0f3761ff8d781
김장관 <minis24@gmail.com>
2023-10-03 21:05:03 +0900

    typescript

--- CustomType/union/union.js
+++ CustomType/union/union.js
@@ -8,36 +8,100 @@ exports.SearchFailedAction = exports.SearchSuccessAction = exports.SearchAction 
 /**
  * 액션타입과 페이로드가 있는 클래스
  */
-class SearchAction {
-    constructor(payload) {
+var SearchAction = /** @class */ (function () {
+    function SearchAction(payload) {
         this.payload = payload;
         this.actionType = "SEARCH";
     }
-}
+    return SearchAction;
+}());
 exports.SearchAction = SearchAction;
-class SearchSuccessAction {
-    constructor(payload) {
+var SearchSuccessAction = /** @class */ (function () {
+    function SearchSuccessAction(payload) {
         this.payload = payload;
         this.actionType = 'SEARCH_SUCCESS';
     }
-}
+    return SearchSuccessAction;
+}());
 exports.SearchSuccessAction = SearchSuccessAction;
 /**
  * 액션타입은 있지만 페이로드가 없는 클래스
  */
-class SearchFailedAction {
-    constructor() {
+var SearchFailedAction = /** @class */ (function () {
+    function SearchFailedAction() {
         this.actionType = "SEARCH_FAILED";
     }
-}
+    return SearchFailedAction;
+}());
 exports.SearchFailedAction = SearchFailedAction;
 function area(shape) {
     switch (shape.kind) {
         case 'rectangle': return shape.width * shape.height;
-        case 'circle': return Math.PI * shape.radius ** 2;
+        case 'circle': return Math.PI * Math.pow(shape.radius, 2);
     }
 }
-const myRectangle = { kind: 'rectangle', width: 10, height: 10 };
-const myCircle = { kind: 'circle', radius: 10 };
-console.log(`Rectangle Area is ${area(myRectangle)}`);
-console.log(`Circle Area is ${area(myCircle)}`);
+var myRectangle = { kind: 'rectangle', width: 10, height: 10 };
+var myCircle = { kind: 'circle', radius: 10 };
+console.log("Rectangle Area is ".concat(area(myRectangle)));
+console.log("Circle Area is ".concat(area(myCircle)));
+function call(param) {
+    if ('a' in param) {
+        return param.a;
+    }
+    return param.b;
+}
+var x = { a: 123 };
+console.log('x :: ', call(x));
+var y = { b: 'bbbbb' };
+console.log('y :: ', call(y));
+var person1;
+person1 = JSON.parse('{"adress" : "25 Broadway"}');
+console.log(person1.address); // adress 오타로 undefined 발생
+/* [출력]
+undefined
+*/
+var person2;
+person2 = JSON.parse('{"adress" : "25 Broadway"}');
+//console.log(person2.address); // unknown 타입인 person2 를 사용했기때문에 컴파일도 되지 않음.
+//타입스크립트는 사용자가 정의한 타입 가드로 객체의 특정 타입을 확인 할 수 있다.
+//const isPerson = (object:any): object is Person => 'address' in object; 
+var isPerson = function (object) { return !!object && 'address' in object; };
+if (isPerson(person2)) {
+    console.log(person2.address);
+}
+else {
+    console.log('person2 is not a Person');
+}
+/* [출력]
+person2 is not a Person
+*/
+if (isPerson(person1)) {
+    console.log(person1.address);
+}
+else {
+    console.log('person1 is not a Person');
+}
+/* [출력]
+person1 is not a Person
+*/
+var person3 = { address: 'ssssss' };
+if (isPerson(person3)) {
+    console.log(person3.address);
+}
+else {
+    console.log('person3 is not a Person');
+}
+/* [출력]
+ssssss
+*/
+isPerson(null);
+function isString(test) {
+    return typeof test === "string";
+}
+function example(foo) {
+    if (isString(foo)) {
+        console.log("it is a string" + foo);
+        console.log(foo.length); // string function
+    }
+}
+example("hello world");
--- CustomType/union/union.ts
+++ CustomType/union/union.ts
@@ -72,3 +72,116 @@ const myCircle: Circle = {kind:'circle',radius:10};
 
 console.log(`Rectangle Area is ${area(myRectangle)}`);
 console.log(`Circle Area is ${area(myCircle)}`);
+
+
+
+//타입가드 in 
+// --> 타입가드 in 키워드는 타입 범위를 축소하는 표현.
+interface IA {
+    a:number;
+}
+
+interface IB {
+    b: string;
+}
+
+function call (param : IA | IB){
+    if ('a' in param){
+        return param.a;
+    }
+    return param.b;
+}
+
+let x:IA = {a:123}
+console.log('x :: ',call(x));
+
+
+let y:IB = {b:'bbbbb'}
+console.log('y :: ',call(y));
+
+
+
+
+//타입 any 사용
+type Person = {
+    descriminator : 'person' ;
+    address: string;
+}
+
+let person1: any;
+person1 = JSON.parse('{"adress" : "25 Broadway"}');
+
+console.log(person1.address); // adress 오타로 undefined 발생
+
+/* [출력]
+undefined
+*/
+
+
+let person2:unknown ;
+person2 = JSON.parse('{"adress" : "25 Broadway"}');
+//console.log(person2.address); // unknown 타입인 person2 를 사용했기때문에 컴파일도 되지 않음.
+
+
+//타입스크립트는 사용자가 정의한 타입 가드로 객체의 특정 타입을 확인 할 수 있다.
+//const isPerson = (object:any): object is Person => 'address' in object; 
+//const isPerson = (object:any): object is Person => !!object && 'address' in object; 
+
+/*
+위에서는 address 프로터티의 유무만으로 Person 타입을 식별할 수 있었는데…경우에 따라 한가지 프로퍼티만으로는 식별하기에 부족한 경우가 있다. 예를들어 , 다른 클래스에 address 프로퍼티가 있는경우….
+
+객체가 특정 타입에 매칭되는지를 확인하려면 몇가지 프로퍼티가 더 필요하다. 
+더 간단한 방법으로는 식별자 프로퍼티를 가지고 타입을 식별하는것.
+*/
+const isPerson = (object:any): object is Person => !!object && object.descriminator === 'person'; 
+
+
+if(isPerson(person2)){
+    console.log(person2.address)
+}
+else{
+    console.log('person2 is not a Person');
+}
+/* [출력]
+person2 is not a Person
+*/
+
+if(isPerson(person1)){
+    console.log(person1.address)
+}
+else{
+    console.log('person1 is not a Person');
+}
+/* [출력]
+person1 is not a Person
+*/
+
+
+let person3 = {address:'ssssss'}
+if(isPerson(person3)){
+    console.log(person3.address)
+}
+else{
+    console.log('person3 is not a Person');
+}
+/* [출력]
+ssssss
+*/
+
+isPerson(null)
+
+
+function isString(test: any): test is string{
+    return typeof test === "string";
+}
+
+function example(foo: any){
+    if(isString(foo)){
+        console.log("it is a string" + foo);
+        console.log(foo.length); // string function
+    }
+}
+example("hello world");
+
+
+
